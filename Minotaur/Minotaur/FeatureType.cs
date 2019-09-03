1bf482f520fd9ffeb8dcfb68a05d0ac202e823d6
Mirandatz <Mirandatz@users.noreply.github.com>
2019-09-03 14:48:32 -0300

    Added xmldocs to FeatureType

--- Minotaur/Minotaur/FeatureType.cs
+++ Minotaur/Minotaur/FeatureType.cs
@@ -1,5 +1,18 @@
 namespace Minotaur {
 
+	/// <summary>
+	/// Since all values in a <see cref="Minotaur.Collections.Dataset.Dataset"/>
+	/// are stored as floats, including values of categorical features,
+	/// this is enum is used to indicate how a dataset column (representing a feature)
+	/// should be treated.
+	/// 
+	/// The value <see cref="FeatureType.Categorical"/> indicates that
+	/// the feature has a categorical nature (e.g. hair color) 
+	/// and must be treated as such.
+	/// For instance, two values from a column marked with
+	/// <see cref="FeatureType.Categorical"/>
+	/// should not be compared with the "less than operator".
+	/// </summary>
 	public enum FeatureType {
 		Categorical,
 		Continuous
