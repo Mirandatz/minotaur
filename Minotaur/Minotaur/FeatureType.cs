ac110bf935c3e4187df5b28edc86cdce9cdaf23e
Mirandatz <Mirandatz@users.noreply.github.com>
2019-09-03 15:19:55 -0300

    Added two new values to FeatureType

--- Minotaur/Minotaur/FeatureType.cs
+++ Minotaur/Minotaur/FeatureType.cs
@@ -12,9 +12,25 @@ namespace Minotaur {
 	/// For instance, two values from a column marked with
 	/// <see cref="FeatureType.Categorical"/>
 	/// should not be compared with the "less than operator".
+	/// 
+	/// The value <see cref="FeatureType.CategoricalButTriviallyValued"/> indicates
+	/// that the featuyre has a categorical nature,
+	/// but contains all instances in the dataset have the same value.
+	/// Usually this is a (undersided) side-effect of the k-fold technique;
+	/// but it may also indicate that the dataset is really well done,
+	/// e.g.: a dataset that for predicting whether a woman has breast cancer
+	/// with the column "IsHuman"... 
+	/// Since all women are human, all instances will have the value "true".
+	/// 
+	/// The value <see cref="FeatureType.ContinuousButTriviallyValued"/> indicates
+	/// that the has a continuous nature,
+	/// but contains all instances in the dataset have the same value.
+	/// Usually this is a (undersided) side-effect of the k-fold technique.
 	/// </summary>
 	public enum FeatureType {
 		Categorical,
-		Continuous
+		Continuous,
+		CategoricalButTriviallyValued,
+		ContinuousButTriviallyValued
 	}
 }
