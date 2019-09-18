329521486e9b6930de8d752092acf67743a03458
Mirandatz <Mirandatz@users.noreply.github.com>
2019-09-18 11:08:54 -0300

    Created new class RepeatingIndividualMutator

---
+++ Minotaur/Minotaur/Theseus/IndividualMutation/RepeatingIndividualMutator.cs
@@ -0,0 +1,41 @@
+namespace Minotaur.Theseus.IndividualMutation {
+	using System;
+	using Minotaur.GeneticAlgorithms.Population;
+
+	public sealed class RepeatingIndividualMutator: IIndividualMutator {
+
+		private readonly IndividualMutator _individualMutator;
+		private const int MaximumMutationCycles = 5;
+
+		public RepeatingIndividualMutator(IndividualMutator individualMutator) {
+			_individualMutator = individualMutator ?? throw new ArgumentNullException(nameof(individualMutator));
+		}
+
+		public bool TryMutate(Individual original, out Individual mutated) {
+
+			var sucess = _individualMutator.TryMutate(original, out var temp);
+			if (!sucess) {
+				mutated = default;
+				return false;
+			}
+
+			var previous = temp;
+			for (int i = 0; i < MaximumMutationCycles; i++) {
+
+				sucess = _individualMutator.TryMutate(
+					original: previous,
+					out var current);
+
+				if (sucess) {
+					previous = current;
+				} else {
+					mutated = previous;
+					return true;
+				}
+			}
+
+			mutated = previous;
+			return true;
+		}
+	}
+}
