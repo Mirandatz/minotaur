cd00c8a13abe96cec5b9e6582193cadf4e787111
MirandaTZ <Mirandatz@users.noreply.github.com>
2019-11-14 12:26:52 -0300

    Created my own "CsbBuilder" class because I couldn't find any library that meets my needs

---
+++ Minotaur/Minotaur/Output/CsvBuilder.cs
@@ -0,0 +1,70 @@
+namespace Minotaur.Output {
+	using System;
+	using System.Linq;
+	using System.Text;
+	using Minotaur.Collections;
+
+	public sealed class CsvBuilder {
+
+		private readonly string _fieldsSeparator;
+		private readonly string _recordSeparator;
+		private readonly Array<string> _fieldNames;
+		private readonly int _fieldsCount;
+
+		private readonly StringBuilder _recordBuilder = new StringBuilder();
+		private readonly StringBuilder _csvBuilder = new StringBuilder();
+
+		private int _fieldsWritten = 0;
+
+		public CsvBuilder(string fieldsSeparator, string recordSeparator, string[] fieldNames) {
+			if (string.IsNullOrWhiteSpace(fieldsSeparator))
+				throw new ArgumentOutOfRangeException(nameof(fieldsSeparator));
+			if (string.IsNullOrWhiteSpace(recordSeparator))
+				throw new ArgumentOutOfRangeException(nameof(recordSeparator));
+			if (fieldsSeparator.Contains(recordSeparator) || recordSeparator.Contains(fieldsSeparator))
+				throw new ArgumentException();
+			if (fieldNames.Length == 0)
+				throw new ArgumentException();
+			if (fieldNames.Any(fn => fn.Contains(fieldsSeparator)))
+				throw new ArgumentException();
+			if (fieldNames.Any(fn => fn.Contains(recordSeparator)))
+				throw new ArgumentException();
+
+			_fieldsSeparator = fieldsSeparator;
+			_recordSeparator = recordSeparator;
+			_fieldNames = fieldNames.ToArray();
+			_fieldsCount = _fieldNames.Length;
+		}
+
+		public void AddField(string value) {
+			if (_fieldsWritten >= _fieldsCount)
+				throw new InvalidOperationException();
+			if (value.Contains(_fieldsSeparator))
+				throw new InvalidOperationException();
+			if (value.Contains(_recordSeparator))
+				throw new InvalidOperationException();
+
+			_recordBuilder.Append(value);
+			_recordBuilder.Append(_fieldsSeparator);
+			_fieldsWritten += 1;
+		}
+
+		public void FinishRecord() {
+			if (_fieldsWritten != _fieldsCount)
+				throw new InvalidOperationException();
+
+			_recordBuilder.Append(_recordSeparator);
+			_csvBuilder.Append(_recordBuilder);
+
+			_recordBuilder.Clear();
+			_fieldsWritten = 0;
+		}
+
+		public string BuildCsv() {
+			if (_fieldsWritten != 0)
+				throw new InvalidOperationException();
+
+			return _csvBuilder.ToString();
+		}
+	}
+}
