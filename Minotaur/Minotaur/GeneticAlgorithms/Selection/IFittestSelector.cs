dde30b8786e5a5c6fe016c8afc091ace186350e0
MirandaTZ <Mirandatz@users.noreply.github.com>
2019-11-26 12:44:10 -0300

    Deleted unused classes

--- Minotaur/Minotaur/GeneticAlgorithms/Selection/IFittestSelector.cs
+++
@@ -1,8 +0,0 @@
-namespace Minotaur.GeneticAlgorithms.Selection {
-	using Minotaur.Collections;
-	using Minotaur.GeneticAlgorithms.Population;
-
-	public interface IFittestSelector {
-		Individual[] SelectFittest(Array<Individual> population);
-	}
-}
