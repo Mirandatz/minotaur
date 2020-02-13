34812207a6e4b52d87fc78222c1d0b7e9e4f4869
Mirandatz <>
2020-02-12 22:51:16 -0300

    Updated CsvBuilder to utilize CsvSeparators

--- Minotaur/Minotaur/Output/CsvBuilder.cs
+++ Minotaur/Minotaur/Output/CsvBuilder.cs
@@ -6,37 +6,31 @@ namespace Minotaur.Output {
 	using System.Text;
 	using Minotaur.Collections;
 
+	/// <remarks>
+	/// This class does not support escape characters.
+	/// </remarks>
 	public sealed class CsvBuilder {
 
-		private readonly string _fieldsSeparator;
-		private readonly string _recordSeparator;
+		private readonly CsvSeparators _separators;
 		private readonly Array<string> _fieldNames;
 		private readonly int _fieldsCount;
 
 		private readonly StringBuilder _recordBuilder;
-		private readonly List<string> _csvBuilder;
+		private readonly List<string> _records;
 
 		private int _fieldsWritten = 0;
 
-		public CsvBuilder(string fieldsSeparator, string recordSeparator, string[] fieldNames) {
-			if (fieldsSeparator != Environment.NewLine && string.IsNullOrEmpty(fieldsSeparator))
-				throw new ArgumentOutOfRangeException(nameof(fieldsSeparator));
-			if (recordSeparator != Environment.NewLine && string.IsNullOrWhiteSpace(recordSeparator))
-				throw new ArgumentOutOfRangeException(nameof(recordSeparator));
-			if (fieldsSeparator.Contains(recordSeparator) || recordSeparator.Contains(fieldsSeparator))
-				throw new ArgumentException();
+		public CsvBuilder(CsvSeparators separators, string[] fieldNames) {
 			if (fieldNames.Length == 0)
 				throw new ArgumentException();
-			if (fieldNames.Any(fn => fn.Contains(fieldsSeparator)))
+			if (fieldNames.Any(fn => separators.IntersectsSeparator(fn)))
 				throw new ArgumentException();
-			if (fieldNames.Any(fn => fn.Contains(recordSeparator)))
+			if (fieldNames.Any(fn => separators.IntersectsSeparator(fn)))
 				throw new ArgumentException();
 
+			_separators = separators;
 			_recordBuilder = new StringBuilder();
-			_csvBuilder = new List<string>();
-
-			_fieldsSeparator = fieldsSeparator;
-			_recordSeparator = recordSeparator;
+			_records = new List<string>();
 			_fieldNames = fieldNames.ToArray();
 			_fieldsCount = _fieldNames.Length;
 
@@ -49,13 +43,11 @@ namespace Minotaur.Output {
 		public void AddField(string value) {
 			if (_fieldsWritten >= _fieldsCount)
 				throw new InvalidOperationException();
-			if (value.Contains(_fieldsSeparator))
-				throw new InvalidOperationException();
-			if (value.Contains(_recordSeparator))
-				throw new InvalidOperationException();
+			if (_separators.IntersectsSeparator(value))
+				throw new ArgumentException();
 
 			if (_fieldsWritten > 0)
-				_recordBuilder.Append(_fieldsSeparator);
+				_recordBuilder.Append(_separators.FieldSeparator);
 
 			_recordBuilder.Append(value);
 			_fieldsWritten += 1;
@@ -65,8 +57,8 @@ namespace Minotaur.Output {
 			if (_fieldsWritten != _fieldsCount)
 				throw new InvalidOperationException();
 
-			_recordBuilder.Append(_recordSeparator);
-			_csvBuilder.Add(_recordBuilder.ToString());
+			_recordBuilder.Append(_separators.RecordSeparator);
+			_records.Add(_recordBuilder.ToString());
 
 			_recordBuilder.Clear();
 			_fieldsWritten = 0;
@@ -79,7 +71,7 @@ namespace Minotaur.Output {
 				throw new InvalidOperationException();
 
 			using var textWritter = new StreamWriter(file);
-			foreach (var line in _csvBuilder)
+			foreach (var line in _records)
 				textWritter.Write(line);
 		}
 	}
