07b183ab6755043bfcc3fab2e42cd2c9c92ce080
Mirandatz <>
2020-02-14 15:54:05 -0300

    PopulationFitnessSerializer is now a sealed class instead of a static class

--- Minotaur/Minotaur/Output/PopulationFitnessSerializer.cs
+++ Minotaur/Minotaur/Output/PopulationFitnessSerializer.cs
@@ -6,21 +6,29 @@ namespace Minotaur.Output {
 	using Minotaur.EvolutionaryAlgorithms;
 	using Minotaur.EvolutionaryAlgorithms.Population;
 
-	public static class PopulationFitnessSerializer {
+	public sealed class PopulationFitnessSerializer {
 
-		public static void SerializeFitnesses(TextWriter textWriter, Array<Individual> population, TrainFitnessEvaluator trainFitnessEvaluator, TestFitnessEvaluator testFitnessEvaluator) {
+		private readonly TrainFitnessEvaluator _trainFitnessEvaluator;
+		private readonly TestFitnessEvaluator _testFitnessEvaluator;
+
+		public PopulationFitnessSerializer(TrainFitnessEvaluator trainFitnessEvaluator, TestFitnessEvaluator testFitnessEvaluator) {
+			_trainFitnessEvaluator = trainFitnessEvaluator;
+			_testFitnessEvaluator = testFitnessEvaluator;
+		}
+
+		public void SerializeFitnesses(TextWriter textWriter, Array<Individual> population) {
 			using var csvWriter = new CsvWriter(writer: textWriter, cultureInfo: CultureInfo.InvariantCulture);
 
 			// csv header
 			csvWriter.WriteField("Id");
-			foreach (var metric in trainFitnessEvaluator.Metrics)
+			foreach (var metric in _trainFitnessEvaluator.Metrics)
 				csvWriter.WriteField($"{metric.Name} (train)");
-			foreach (var metric in testFitnessEvaluator.Metrics)
+			foreach (var metric in _testFitnessEvaluator.Metrics)
 				csvWriter.WriteField($"{metric.Name} (test)");
 			csvWriter.NextRecord();
 
-			var trainFitnesses = trainFitnessEvaluator.EvaluateAsMaximizationTask(population);
-			var testFitnesses = testFitnessEvaluator.EvaluateAsMaximizationTask(population);
+			var trainFitnesses = _trainFitnessEvaluator.EvaluateAsMaximizationTask(population);
+			var testFitnesses = _testFitnessEvaluator.EvaluateAsMaximizationTask(population);
 
 			for (int i = 0; i < population.Length; i++) {
 				csvWriter.WriteField(population[i].Id);
