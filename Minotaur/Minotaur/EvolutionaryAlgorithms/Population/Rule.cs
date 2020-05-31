a41ea0ae24eae73b576f7f5a364537486b78f02b
MirandaTZ <Mirandatz@users.noreply.github.com>
2020-05-31 20:12:19 -0300

    Rule no longer implements IEquatable<Rule>

--- Minotaur/Minotaur/EvolutionaryAlgorithms/Population/Rule.cs
+++ Minotaur/Minotaur/EvolutionaryAlgorithms/Population/Rule.cs
@@ -7,19 +7,13 @@ namespace Minotaur.EvolutionaryAlgorithms.Population {
 
 	// @Assumption: all rules contain Antecedents with the same length.
 	// @Assumption: all rules contain Consequents with the same length.
-	public sealed class Rule: IEquatable<Rule> {
+	public sealed class Rule {
 		public const int MinimumTestCount = 1;
 
 		public readonly int NonNullTestCount;
-
-		/// <summary>
-		/// Contains N references to tests, with N == dataset.FeatureCount.
-		/// The tests are sorted by the feature index they are testing.
-		/// </summary>
 		public readonly Array<IFeatureTest> Antecedent;
 		public readonly ILabel Consequent;
-
-		private readonly int _precomputedHashCode;
+		public readonly int PrecomputedHashCode;
 
 		public Rule(Array<IFeatureTest> antecedent, ILabel consequent) {
 			if (antecedent.ContainsNulls())
@@ -38,7 +32,7 @@ namespace Minotaur.EvolutionaryAlgorithms.Population {
 					throw new ArgumentException(nameof(antecedent) + " must be sorted and can not contain multiple tests for the same feature.");
 			}
 
-			_precomputedHashCode = PrecompileHashcode(antecedent, consequent);
+			PrecomputedHashCode = PrecompileHashcode(antecedent, consequent);
 
 			static int PrecompileHashcode(Array<IFeatureTest> antecedent, ILabel consequent) {
 				var hash = new HashCode();
@@ -64,25 +58,13 @@ namespace Minotaur.EvolutionaryAlgorithms.Population {
 			return true;
 		}
 
-		public override string ToString() {			
+		public override string ToString() {
 			var antecedent = "IF " + string.Join(" AND ", Antecedent);
 			var consequent = " THEN " + Consequent.ToString();
 			return antecedent + consequent;
 		}
 
-		public override int GetHashCode() => _precomputedHashCode;
-
-		public override bool Equals(object? obj) => Equals((Rule) obj!);
-
-		public bool Equals(Rule other) {
-			if (ReferenceEquals(this, other))
-				return true;
-
-			if (_precomputedHashCode != other._precomputedHashCode)
-				return false;
-
-			return Consequent.Equals(other.Consequent) &&
-				Antecedent.SequenceEquals(other.Antecedent);
-		}
+		public override int GetHashCode() => throw new NotImplementedException();
+		public override bool Equals(object? obj) => throw new NotImplementedException();
 	}
 }
