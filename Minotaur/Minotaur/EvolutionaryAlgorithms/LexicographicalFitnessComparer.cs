b2327036479a5d1d52558228ff90d00d9d14971d
MirandaTZ <Mirandatz@users.noreply.github.com>
2020-05-31 20:28:53 -0300

    Fixed issues with nullability attributes in LexicographicalFitnessComparer

--- Minotaur/Minotaur/EvolutionaryAlgorithms/LexicographicalFitnessComparer.cs
+++ Minotaur/Minotaur/EvolutionaryAlgorithms/LexicographicalFitnessComparer.cs
@@ -1,10 +1,14 @@
 namespace Minotaur.EvolutionaryAlgorithms {
 	using System;
 	using System.Collections.Generic;
+	using System.Diagnostics.CodeAnalysis;
 
 	public sealed class LexicographicalFitnessComparer: IComparer<Fitness> {
-
-		public int Compare(Fitness lhs, Fitness rhs) {
+		public int Compare([AllowNull] Fitness lhs, [AllowNull] Fitness rhs) {
+			if (lhs is null)
+				throw new ArgumentNullException(nameof(lhs));
+			if (rhs is null)
+				throw new ArgumentNullException(nameof(rhs));
 			if (lhs.Count != rhs.Count)
 				throw new ArgumentException(nameof(lhs) + " and " + nameof(rhs) + " must have the same length.");
 
