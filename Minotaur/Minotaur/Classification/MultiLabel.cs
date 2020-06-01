78e2a9e663ca7039fcdf37caecc123b6d5cd1cfa
MirandaTZ <Mirandatz@users.noreply.github.com>
2020-06-01 17:47:37 -0300

    Improved argument validation of MultiLabel's constructor

--- Minotaur/Minotaur/Classification/MultiLabel.cs
+++ Minotaur/Minotaur/Classification/MultiLabel.cs
@@ -4,11 +4,16 @@ namespace Minotaur.Classification {
 
 	public sealed class MultiLabel: ILabel {
 
+		private const int MinimumElementCount = 1;
+
 		public readonly Array<bool> Values;
 		public readonly int Length;
 		public readonly int PrecomputedHashCode;
 
 		public MultiLabel(Array<bool> values) {
+			if (values.Length < MinimumElementCount)
+				throw new ArgumentException($"{nameof(values)} must contain at least {MinimumElementCount} elements.");
+
 			Values = values.ShallowCopy();
 			Length = Values.Length;
 
