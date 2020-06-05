385d7a411ecc6f690642cebe5efee7e6c1d7ef2d
MirandaTZ <Mirandatz@users.noreply.github.com>
2020-06-05 11:41:28 -0300

    AverageRuleVolume methods throw NotImplementedException (temporarily)

--- Minotaur/Minotaur/EvolutionaryAlgorithms/Metrics/AverageRuleVolume.cs
+++ Minotaur/Minotaur/EvolutionaryAlgorithms/Metrics/AverageRuleVolume.cs
@@ -1,4 +1,5 @@
 namespace Minotaur.EvolutionaryAlgorithms.Metrics {
+	using System;
 	using Minotaur.Classification;
 	using Minotaur.Collections.Dataset;
 	using Minotaur.EvolutionaryAlgorithms.Population;
@@ -13,55 +14,6 @@ namespace Minotaur.EvolutionaryAlgorithms.Metrics {
 
 		public string Name => nameof(AverageRuleVolume);
 
-		public float EvaluateAsMaximizationTask(Individual individual) {
-			var rules = individual.Rules;
-			double sumOfVolumes = 1;
-
-			for (int i = 0; i < rules.Length; i++)
-				sumOfVolumes += ComputeRuleVolume(rules[i]);
-
-			return (float) (sumOfVolumes / rules.Length);
-		}
-
-		private double ComputeRuleVolume(Rule rule) {
-			double volume = 1;
-
-			var tests = rule.Antecedent;
-			for (int i = 0; i < tests.Length; i++)
-				volume *= ComputeTestVolume(rule, i);
-
-			return volume;
-		}
-
-		private double ComputeTestVolume(Rule rule, int testIndex) {
-			var test = rule.Antecedent[testIndex];
-			var featureType = _dataset.GetFeatureType(featureIndex: testIndex);
-
-			return featureType switch
-			{
-				FeatureType.Continuous => ComputeContinuousFeatureTestVolume(test, testIndex),
-
-				_ => throw CommonExceptions.UnknownFeatureType
-			};
-		}
-
-		private double ComputeContinuousFeatureTestVolume(IFeatureTest test, int testIndex) {
-			if (test is ContinuousFeatureTest cft) {
-				var lower = cft.LowerBound;
-				var upper = cft.UpperBound;
-
-				var featureValues = _dataset.GetSortedUniqueFeatureValues(featureIndex: testIndex);
-
-				if (float.IsNegativeInfinity(lower))
-					lower = featureValues[0];
-
-				if (float.IsPositiveInfinity(upper))
-					upper = featureValues[^1];
-
-				return upper - lower;
-			}
-
-			throw CommonExceptions.UnknownFeatureTestImplementation;
-		}
+		public float EvaluateAsMaximizationTask(Individual individual) => throw new NotImplementedException();
 	}
 }
