76ab8f9571de44355a7efb4728aec101c8785cf4
MirandaTZ <Mirandatz@users.noreply.github.com>
2020-06-05 11:42:22 -0300

    ModelSize  methods throw NotImplementedException (temporarily)

--- Minotaur/Minotaur/EvolutionaryAlgorithms/Metrics/ModelSize.cs
+++ Minotaur/Minotaur/EvolutionaryAlgorithms/Metrics/ModelSize.cs
@@ -1,28 +1,12 @@
 namespace Minotaur.EvolutionaryAlgorithms.Metrics {
+	using System;
 	using Minotaur.EvolutionaryAlgorithms.Population;
 
 	public sealed class ModelSize: IMetric {
 
 		public string Name => nameof(ModelSize);
 
-		public float Evaluate(Individual individual) {
-			var modelSize = 0;
-			var rules = individual.Rules;
-			for (int i = 0; i < rules.Length; i++)
-				modelSize += RuleSize(rules[i]);
-
-			return modelSize;
-		}
-
-		private int RuleSize(Rule rule) {
-			var ruleSize = 0;
-			var tests = rule.Antecedent;
-
-			for (int i = 0; i < tests.Length; i++)
-				ruleSize += tests[i].TestSize;
-
-			return ruleSize;
-		}
+		public float Evaluate(Individual individual) => throw new NotImplementedException();
 
 		public float EvaluateAsMaximizationTask(Individual individual) => -1 * Evaluate(individual);
 	}
