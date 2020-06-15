b49fa2278cef309aa2cc97669ac2aa0a3070f136
MirandaTZ <Mirandatz@users.noreply.github.com>
2020-06-15 16:18:22 -0300

    Created 2 new classes InstanceFeatures and InstanceLabels

---
+++ Minotaur/Minotaur/Datasets/InstanceFeatures.cs
@@ -0,0 +1,66 @@
+namespace Minotaur.Datasets {
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Diagnostics.CodeAnalysis;
+	using Minotaur.ExtensionMethods.SystemArray;
+
+	public sealed class InstanceDate: IEquatable<InstanceDate>, IReadOnlyList<float> {
+
+		private readonly float[] _values;
+		private readonly int _precomputedHashCode;
+
+		public InstanceDate(ReadOnlySpan<float> values) {
+
+			var storage = new float[values.Length];
+			var hash = new HashCode();
+
+			for (int i = 0; i < values.Length; i++) {
+				var v = values[i];
+
+				if (float.IsNaN(v))
+					throw new ArgumentException(nameof(values) + " can't contain NaNs.");
+				if (float.IsInfinity(v))
+					throw new ArgumentException(nameof(values) + " can't contain Infinity.");
+
+				storage[i] = v;
+				hash.Add(v);
+			}
+
+			_values = storage;
+			_precomputedHashCode = hash.ToHashCode();
+		}
+
+		public ReadOnlySpan<float> AsSpan() => _values.AsSpan();
+
+		public override string ToString() => throw new NotImplementedException();
+
+		public override int GetHashCode() => _precomputedHashCode;
+
+		public override bool Equals(object? obj) => Equals((InstanceDate) obj!);
+
+		public bool Equals([AllowNull] InstanceDate other) {
+			if (other is null)
+				throw new ArgumentNullException(nameof(other));
+
+			if (ReferenceEquals(this, other))
+				return true;
+
+			var lhs = this._values.AsSpan();
+			var rhs = other._values.AsSpan();
+
+			if (lhs.Length != rhs.Length)
+				throw new InvalidOperationException();
+
+			return lhs.SequenceEqual(rhs);
+		}
+
+		public float this[int index] => _values[index];
+
+		public int Count => _values.Length;
+
+		public IEnumerator<float> GetEnumerator() => _values.GetGenericEnumerator();
+
+		IEnumerator IEnumerable.GetEnumerator() => _values.GetEnumerator();
+	}
+}
---
+++ Minotaur/Minotaur/Datasets/InstanceLabels.cs
@@ -0,0 +1,60 @@
+namespace Minotaur.Datasets {
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Diagnostics.CodeAnalysis;
+	using ExtensionMethods.SystemArray;
+
+	public sealed class InstanceLabels: IEquatable<InstanceLabels>, IReadOnlyList<bool> {
+
+		private readonly bool[] _values;
+		private readonly int _precomputedHashCode;
+
+		public InstanceLabels(ReadOnlySpan<bool> values) {
+
+			var storage = new bool[values.Length];
+			var hash = new HashCode();
+
+			for (int i = 0; i < values.Length; i++) {
+				var v = values[i];
+				storage[i] = v;
+				hash.Add(v);
+			}
+
+			_values = storage;
+			_precomputedHashCode = hash.ToHashCode();
+		}
+
+		public ReadOnlySpan<bool> AsSpan() => _values.AsSpan();
+
+		public override string ToString() => throw new NotImplementedException();
+
+		public override int GetHashCode() => _precomputedHashCode;
+
+		public override bool Equals(object? obj) => Equals((InstanceLabels) obj!);
+
+		public bool Equals([AllowNull] InstanceLabels other) {
+			if (other is null)
+				throw new ArgumentNullException(nameof(other));
+
+			if (ReferenceEquals(this, other))
+				return true;
+
+			var lhs = this._values.AsSpan();
+			var rhs = other._values.AsSpan();
+
+			if (lhs.Length != rhs.Length)
+				throw new InvalidOperationException();
+
+			return lhs.SequenceEqual(rhs);
+		}
+
+		public int Count => _values.Length;
+
+		public bool this[int index] => _values[index];
+
+		public IEnumerator<bool> GetEnumerator() => _values.GetGenericEnumerator();
+
+		IEnumerator IEnumerable.GetEnumerator() => _values.GetEnumerator();
+	}
+}
