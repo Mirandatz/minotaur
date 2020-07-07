2710d8f50b2301532eb24b9e46d566f426d72887
Mirandatz <Mirandatz@users.noreply.github.com>
2020-07-07 17:56:45 -0300

    Implemented InstancesFeaturesManager's Create() method

--- Minotaur/Minotaur/Datasets/InstancesFeaturesManager.cs
+++ Minotaur/Minotaur/Datasets/InstancesFeaturesManager.cs
@@ -1,5 +1,6 @@
 namespace Minotaur.Datasets {
 	using System;
+	using System.Collections.Generic;
 
 	public sealed class InstancesFeaturesManager {
 
@@ -8,12 +9,45 @@ namespace Minotaur.Datasets {
 		private readonly InstanceFeatures[] _instanceFeatures;
 
 		// Constructors and alike
-		public InstancesFeaturesManager() {
-			_instanceFeatures = null!;
-			throw new NotImplementedException();
+		private InstancesFeaturesManager(int instanceCount, int featureCount, InstanceFeatures[] instanceFeatures) {
+			InstanceCount = instanceCount;
+			FeatureCount = featureCount;
+			_instanceFeatures = instanceFeatures;
 		}
 
-		public static InstancesFeaturesManager Create(ReadOnlySpan<InstanceFeatures> instancesFeatures) => throw new NotImplementedException();
+		public static InstancesFeaturesManager Create(ReadOnlySpan<InstanceFeatures> instancesFeatures) {
+			if (instancesFeatures.Length == 0)
+				throw new ArgumentException(nameof(instancesFeatures) + " can't be empty.");
+
+			var storage = new InstanceFeatures[instancesFeatures.Length];
+			var uniques = new HashSet<InstanceFeatures>(instancesFeatures.Length);
+			var expectedFeatureCount = instancesFeatures[0].Count;
+
+			// Checking if all instances have the same number of features
+			// and if there are any duplicated instances
+			for (int i = 0; i < instancesFeatures.Length; i++) {
+				var current = instancesFeatures[i];
+
+				if (current.Count != expectedFeatureCount) {
+					throw new ArgumentException(nameof(instancesFeatures) + " " +
+						"contains instances with " +
+						"different numbers of features.");
+				}
+
+				var isUnique = uniques.Add(current);
+				if (!isUnique) {
+					throw new ArgumentException(nameof(instancesFeatures) + " " +
+						"contains duplicated instances.");
+				}
+
+				storage[i] = current;
+			}
+
+			return new InstancesFeaturesManager(
+				instanceCount: storage.Length,
+				featureCount: expectedFeatureCount,
+				instanceFeatures: storage);
+		}
 
 		// Actual methods
 		public InstanceFeatures GetFeatures(int instanceIndex) {
