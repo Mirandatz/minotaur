88b9373a90bcdf4d72fc2bb3bf51999628a852f1
Mirandatz <Mirandatz@users.noreply.github.com>
2020-07-15 18:38:16 -0300

    Removed unused methods from IndexingHelper

--- Minotaur/Minotaur/Collections/IndexingHelper.cs
+++ Minotaur/Minotaur/Collections/IndexingHelper.cs
@@ -3,34 +3,13 @@ namespace Minotaur.Collections {
 
 	public static class IndexingHelper {
 
-		public static T[] CopyIndexedItems<T>(Array<int> indices, Array<T> items) {
-			var alreadyCopied = new bool[indices.Length];
-			var indexedItems = new T[indices.Length];
+		public static int[] CreateIndices(int count) {
+			if (count <= 0)
+				throw new ArgumentOutOfRangeException(nameof(count));
 
-			for (int i = 0; i < indexedItems.Length; i++) {
-				if (alreadyCopied[i])
-					throw new InvalidOperationException();
-
-				indexedItems[i] = items[indices[i]];
-				alreadyCopied[i] = true;
-			}
-
-			return indexedItems;
-		}
-
-		public static IndexedItem<T>[] IndexItems<T>(Array<T> items) where T : class {
-			var indexedItems = new IndexedItem<T>[items.Length];
-
-			for (int i = 0; i < indexedItems.Length; i++)
-				indexedItems[i] = new IndexedItem<T>(i, items[i]);
-
-			return indexedItems;
-		}
-
-		public static int[] ExtractIndices<T>(Array<IndexedItem<T>> indexedItems) where T : class {
-			var indices = new int[indexedItems.Length];
+			var indices = new int[count];
 			for (int i = 0; i < indices.Length; i++)
-				indices[i] = indexedItems[i].Index;
+				indices[i] = i;
 
 			return indices;
 		}
