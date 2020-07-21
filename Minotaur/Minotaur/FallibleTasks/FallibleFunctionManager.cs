4205cd2003227a4aa6284e3dd3c31a4ac2f3fea8
Mirandatz <Mirandatz@users.noreply.github.com>
2020-07-21 05:28:20 -0300

    Moved parallel execution of fallible functions from PopulationMutator to a new class

---
+++ Minotaur/Minotaur/FallibleTasks/FallibleFunctionManager.cs
@@ -0,0 +1,47 @@
+namespace Minotaur.FallibleTasks {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	public static class FallibleFunctionManager {
+
+		public static T[]? TryGenerateDistinctResults<T>(int maxFailedAttempts, int targetResultCount, Func<T?> fallibleFunction) where T : class, IEquatable<T> {
+			if (maxFailedAttempts < 0)
+				throw new ArgumentOutOfRangeException(nameof(maxFailedAttempts) + " must be >= 0.");
+			if (targetResultCount <= 0)
+				throw new ArgumentOutOfRangeException(nameof(targetResultCount) + " must be > 0.");
+
+			var distinctResults = new HashSet<T>(capacity: targetResultCount);
+
+			var loopController = new LoopController<T>(
+				maxFailedAttempts: maxFailedAttempts,
+				targetResultCount: targetResultCount,
+				distinctResults: distinctResults);
+
+			var tasks = new Task[Environment.ProcessorCount];
+
+			for (int i = 0; i < tasks.Length; i++) {
+				tasks[i] = Task.Run(() => RunFallibleTask(
+					loopController: loopController,
+					fallibleFunction: fallibleFunction));
+			}
+
+			Task.WaitAll(tasks);
+
+			if (distinctResults.Count < targetResultCount)
+				return null;
+			if (distinctResults.Count == targetResultCount)
+				return distinctResults.ToArray();
+			else
+				return distinctResults.Take(targetResultCount).ToArray();
+		}
+
+		private static void RunFallibleTask<T>(LoopController<T> loopController, Func<T?> fallibleFunction) where T : class, IEquatable<T> {
+			while (loopController.ShouldContinueLooping) {
+				var result = fallibleFunction();
+				loopController.UpdateLoopStatus(result);
+			}
+		}
+	}
+}
--- Minotaur/Minotaur/Mutation/PopulationMutator.cs
+++ Minotaur/Minotaur/Mutation/PopulationMutator.cs
@@ -1,12 +1,8 @@
 namespace Minotaur.Mutation {
 	using System;
-	using System.Collections.Generic;
 	using System.Linq;
-	using System.Threading;
-	using System.Threading.Tasks;
 	using Minotaur.Classification;
 	using Minotaur.Collections;
-	using Random = Random.ThreadStaticRandom;
 
 	public sealed class PopulationMutator {
 
@@ -31,44 +27,7 @@ namespace Minotaur.Mutation {
 			if (population.ToHashSet().Count != population.Length)
 				throw new ArgumentException(nameof(population) + " can't contain duplicated elements.");
 
-			var uniqueModels = GenerateMutantsInParallel(population);
-			var mutantCount = uniqueModels.Count - population.Length;
-
-			// Dang it, we couldn't generate the target number of mutants
-			if (mutantCount < _targetNumberOfMutants)
-				return null;
-
-			// Removing non-mutants, i.e. individuals of the original population
-			uniqueModels.ExceptWith(population);
-
-			// Our mutant generation stratengy may generate a few more morants than _targetNumberOfMutants
-			if (uniqueModels.Count == _targetNumberOfMutants)
-				return uniqueModels.ToArray();
-			else
-				return uniqueModels.Take(_targetNumberOfMutants).ToArray();
-		}
-
-		private HashSet<ConsistentModel> GenerateMutantsInParallel(Array<ConsistentModel> population) {
-			var loopControl = new LoopControl(
-				previousGenerationPopulation: population,
-				targetNumberOfMutants: _targetNumberOfMutants,
-				maximumFailedMutationAttempts: _maximumFailedAttempts);
-
-			ThreadPool.GetMaxThreads(out var workerThreads, out var _);
-			var tasks = new Task[workerThreads];
-			for (int i = 0; i < tasks.Length; i++)
-				tasks[i] = Task.Run(() => GenerateMutantsTask(loopControl));
-
-			Task.WaitAll(tasks);
-			return loopControl.UniqueModels;
-		}
-
-		private void GenerateMutantsTask(LoopControl loopControl) {
-			while (loopControl.ContinueLooping) {
-				var candidate = Random.Choice(loopControl.PreviousGenerationPopulation);
-				var mutant = _individualMutator.TryMutate(candidate);
-				Task.Run(() => loopControl.UpdateState(mutant));
-			}
+			throw new NotImplementedException();
 		}
 
 		// Silly overrides
@@ -77,59 +36,5 @@ namespace Minotaur.Mutation {
 		public override int GetHashCode() => throw new NotImplementedException();
 
 		public override bool Equals(object? obj) => throw new NotImplementedException();
-
-		private sealed class LoopControl {
-
-			private readonly int _maximumFailedAttempts;
-			private readonly int _populationSize;
-			private readonly int _targetNumberOfMutants;
-			private int _failedAttempts = 0;
-			public readonly Array<ConsistentModel> PreviousGenerationPopulation;
-			public readonly HashSet<ConsistentModel> UniqueModels;
-			public bool ContinueLooping = true;
-
-			public LoopControl(Array<ConsistentModel> previousGenerationPopulation, int targetNumberOfMutants, int maximumFailedMutationAttempts) {
-				PreviousGenerationPopulation = previousGenerationPopulation;
-				UniqueModels = new HashSet<ConsistentModel>(capacity: previousGenerationPopulation.Length + targetNumberOfMutants);
-				for (int i = 0; i < previousGenerationPopulation.Length; i++)
-					UniqueModels.Add(previousGenerationPopulation[i]);
-
-				_populationSize = previousGenerationPopulation.Length;
-				_maximumFailedAttempts = maximumFailedMutationAttempts;
-				_targetNumberOfMutants = targetNumberOfMutants;
-			}
-
-			public void UpdateState(ConsistentModel? mutant) {
-				if (mutant is null) {
-					var updatedFailAttempts = Interlocked.Increment(ref _failedAttempts);
-					ContinueLooping = updatedFailAttempts < _maximumFailedAttempts;
-					return;
-				}
-
-				bool isUnique;
-				int uniqueCount;
-				lock (UniqueModels) {
-					isUnique = UniqueModels.Add(mutant);
-					uniqueCount = UniqueModels.Count;
-				}
-
-				if (!isUnique) {
-					var updatedFailAttempts = Interlocked.Increment(ref _failedAttempts);
-					ContinueLooping = updatedFailAttempts < _maximumFailedAttempts;
-					return;
-				}
-
-				var mutantCount = uniqueCount - _populationSize;
-				if (mutantCount >= _targetNumberOfMutants)
-					ContinueLooping = false;
-			}
-
-			// Silly overrides
-			public override string ToString() => throw new NotImplementedException();
-
-			public override int GetHashCode() => throw new NotImplementedException();
-
-			public override bool Equals(object? obj) => throw new NotImplementedException();
-		}
 	}
 }
